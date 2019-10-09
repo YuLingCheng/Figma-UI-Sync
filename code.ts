0452b49ed2ab88782380bd895049f71dbfb26140
Adèle Gauvrit <adele.gauvrit-ext@dashlane.com>
2019-10-09 17:42:36 +0200

    Parse figma paintstyles to object

--- code.ts
+++ code.ts
@@ -8,6 +8,50 @@
 // This shows the HTML page in "ui.html".
 figma.showUI(__html__);
 
+// ------ Parse paintstyles -------
+const painStyles = figma.getLocalPaintStyles();
+
+const parseColorName = (
+  color: PaintStyle
+): { name: string; variant: number } => {
+  const [name, variant] = color.name.split(" / ");
+  return {
+    name: name.toLowerCase(),
+    variant: parseInt(variant)
+  };
+};
+
+const parseRGBToHex = (color: RGB): string => {
+  const r = Math.floor(color.r * 255);
+  const g = Math.floor(color.g * 255);
+  const b = Math.floor(color.b * 255);
+  return `#${r.toString(16)}${g.toString(16)}${b.toString(16)}`;
+};
+
+const solidPaintStyles = painStyles.filter(
+  paintStyle => paintStyle.paints[0].type === "SOLID"
+);
+
+interface ParsedColors {
+  [colorName: string]: {
+    [colorVariant: number]: string;
+  };
+}
+
+const reducedColors = {};
+solidPaintStyles.reduce((reducedColors: ParsedColors, solidPainstyle) => {
+  const { name, variant } = parseColorName(solidPainstyle);
+  if (!reducedColors[name]) {
+    reducedColors[name] = {};
+  }
+
+  const paint = solidPainstyle.paints[0] as SolidPaint;
+  reducedColors[name][variant] = parseRGBToHex(paint.color);
+  return reducedColors;
+}, reducedColors);
+
+console.log(reducedColors);
+
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
 // posted message.
