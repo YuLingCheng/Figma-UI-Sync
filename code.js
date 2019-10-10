e9d51eab4a10a232b360abd838323a2a0743cee7
Adèle Gauvrit <adele.gauvrit-ext@dashlane.com>
2019-10-10 10:37:18 +0200

    Save and restore UX name

--- code.js
+++ code.js
@@ -1,10 +1,28 @@
 // This plugin will open a modal to prompt the user to enter a number, and
 // it will then create that many rectangles on the screen.
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 // This file holds the main code for the plugins. It has access to the *document*.
 // You can access browser APIs in the <script> tag inside "ui.html" which has a
 // full browser enviroment (see documentation).
 // This shows the HTML page in "ui.html".
 figma.showUI(__html__);
+// get designer's name
+const getDesignersName = () => __awaiter(this, void 0, void 0, function* () {
+    const name = yield figma.clientStorage.getAsync("UX_NAME");
+    figma.ui.postMessage({
+        type: "GET_STORED_UX_NAME",
+        name
+    });
+});
+getDesignersName();
 // ------ Parse paintstyles -------
 const painStyles = figma.getLocalPaintStyles();
 const parseColorName = (color) => {
@@ -31,14 +49,15 @@ solidPaintStyles.reduce((reducedColors, solidPainstyle) => {
     reducedColors[name][variant] = parseRGBToHex(paint.color);
     return reducedColors;
 }, reducedColors);
-console.log(reducedColors);
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
 // posted message.
 figma.ui.onmessage = msg => {
     // One way of distinguishing between different types of messages sent from
     // your HTML page is to use an object with a "type" property like this.
-    if (msg.type === "push-changes") {
-        const { token } = msg;
+    console.log(msg);
+    if (msg.type === "SAVE_UX_NAME") {
+        const { uxName } = msg;
+        figma.clientStorage.setAsync("UX_NAME", uxName);
     }
 };
